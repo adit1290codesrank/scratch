3464e15c0f666907f046b328dbac52f1b4d67310
Adit <adit.mohanty@gmail.com>
2026-09-02 20:29:44 +0530

    LR Scheduling

--- include/core/optimizer.h
+++ include/core/optimizer.h
@@ -29,4 +29,6 @@ class Adam:public Optimizer
 
         Adam(std::vector<Layer*> layers,float lr=0.001f,float beta1=0.9f,float beta2=0.999f,float eps=1e-8f);
         void step() override;
+
+        void set_lr(float new_lr){this->lr=new_lr;}
 };
